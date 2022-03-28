c029c4e10b3fa89fd064271b746a3ef46dca7d38
cetinnetwork <74578023+cetinnetwork@users.noreply.github.com>
2022-03-28 08:51:16 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,9 +4,6 @@ sudo apt-get upgrade -y
 
 sudo apt-get dist-upgrade -y
 
-sudo apt-get install python3 tesseract-ocr-all npm default-jdk powershell wget git vlc kodi virtualbox r-base ukuu
-grub-customizer -y
-
 pip install -r requirements.txt
 
 pip install --upgrade pip
