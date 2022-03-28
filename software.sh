ce3fad6f724cfc8e065011a37ffe40eb58f33955
cetinnetwork <74578023+cetinnetwork@users.noreply.github.com>
2022-03-28 08:52:09 +0200

    Create software.sh

---
+++ software.sh
@@ -0,0 +1,16 @@
+sudo apt-get install python 
+sudo apt-get install tesseract-ocr-all -y 
+sudo apt-get install npm -y
+sudo apt-get install default-jdk -y
+sudo apt-get install powershell -y 
+sudo apt-get install wget -y
+sudo apt-get install git -y
+sudo apt-get install vlc -y
+sudo apt-get install kodi -y
+sudo apt-get install virtualbox -y 
+sudo apt-get install r-base -y
+sudo apt-get install ukuu -y 
+sudo apt-get install grub-customizer -y
+sudo apt-get install teams -y
+sudo apt-get install git -y
+sudo apt-get install ocr-feeder -y
